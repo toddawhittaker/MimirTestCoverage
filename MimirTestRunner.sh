c196c7fcac52d9d8bba810dc8dbe2b032e66c812
toddawhittaker <todd@thewhittakers.org>
2021-01-10 19:44:32 -0500

    Updating shellcheck fixes

--- MimirTestRunner.sh
+++ MimirTestRunner.sh
@@ -29,37 +29,37 @@ javac -d bin *.java;
 
 # compile error triggers zero grade
 if [ $? -ne 0 ]; then
-  echo 0 > ${DIR}/OUTPUT
+  echo 0 > "${DIR}/OUTPUT"
   exit 1
 fi
 
 # produce any student output for their own testing
 if [ -f "RunMe.java" ]; then
-    java RunMe >> ${DIR}/DEBUG
+    java RunMe >> "${DIR}/DEBUG"
 fi
 
 # run test cases instrumented with JaCoCo
-java -javaagent:lib/jacocoagent.jar=destfile=results.exec MimirTestRunner >> ${DIR}/DEBUG
+java -javaagent:lib/jacocoagent.jar=destfile=results.exec MimirTestRunner >> "${DIR}/DEBUG"
 status=$?
 if [ $status -ne 100 ]; then
-  echo 0 > ${DIR}/OUTPUT
-  echo "All test cases MUST pass. Your score is 0%" >> ${DIR}/DEBUG
+  echo 0 > "${DIR}/OUTPUT"
+  echo "All test cases MUST pass. Your score is 0%" >> "${DIR}/DEBUG"
   exit 1
 fi
 
 # produce JaCoCo report as CSV
-java -jar lib/jacococli.jar report results.exec --classfiles bin --csv results.csv --sourcefiles . >> ${DIR}/DEBUG
+java -jar lib/jacococli.jar report results.exec --classfiles bin --csv results.csv --sourcefiles . --quiet #>> ${DIR}/DEBUG
 if [ ! -f results.csv ]; then
-  echo 0 > ${DIR}/OUTPUT
-  echo "No coverage results generated" >> ${DIR}/DEBUG
+  echo 0 > "${DIR}/OUTPUT"
+  echo "No coverage results generated" >> "${DIR}/DEBUG"
   exit 1
 fi
 
 # ugly Bash processing of CSV file
 total=0;
 covered=0;
-echo "" >> ${DIR}/DEBUG
-echo "Coverage results:" >> ${DIR}/DEBUG
+echo "" >> "${DIR}/DEBUG"
+echo "Coverage results:" >> "${DIR}/DEBUG"
 
 IFS=','
 while read GROUP PACKAGE CLASS INSTRUCTION_MISSED \
@@ -71,11 +71,11 @@ do
   if [[ ! ${CLASS} =~ .+Test(Runner)?$ ]]; then
     class_lines=$(($LINE_MISSED+$LINE_COVERED))
     class_branch=$(($BRANCH_MISSED+$BRANCH_COVERED))
-    covered=$(($covered+$LINE_COVERED+BRANCH_COVERED))
+    covered=$(($covered+$LINE_COVERED+$BRANCH_COVERED))
     total=$(($total+$class_lines+$class_branch))
-    echo "  Class $CLASS" >> ${DIR}/DEBUG
-    echo "    $LINE_COVERED of $class_lines lines covered" >> ${DIR}/DEBUG
-    echo "    $BRANCH_COVERED of $class_branch branches covered" >> ${DIR}/DEBUG
+    echo "  Class $CLASS" >> "${DIR}/DEBUG"
+    echo "    $LINE_COVERED of $class_lines lines covered" >> "${DIR}/DEBUG"
+    echo "    $BRANCH_COVERED of $class_branch branches covered" >> "${DIR}/DEBUG"
   fi
 done < <(tail -n +2 results.csv)
 
@@ -84,8 +84,8 @@ if [ $total -ne 0 ]; then
   pct=$(($covered*100/$total))
 fi
 
-echo "" >> ${DIR}/DEBUG
-echo "$covered of $total elements covered. Your score is $pct%" >> ${DIR}/DEBUG
+echo "" >> "${DIR}/DEBUG"
+echo "$covered of $total elements covered. Your score is $pct%" >> "${DIR}/DEBUG"
 
 # produce final grade
-echo "$pct" > ${DIR}/OUTPUT
+echo "$pct" > "${DIR}/OUTPUT"
